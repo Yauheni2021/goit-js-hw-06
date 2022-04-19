aef01e6ed0b1e6473e2b4d9983c36f04c4ad038a
Yauheni Kaspiarovich <kaspiarovichy@gmail.com>
2022-04-19 09:29:29 +0200

    Add task 2

--- js/task-02.js
+++ js/task-02.js
@@ -7,4 +7,13 @@ const ingredients = [
   'Condiments',
 ];
 
-const ingredientsEl = document.createElement();
+const ingridientsRef = ingredients.map(element => {
+  const itemRef = document.createElement('li');
+  itemRef.textContent = element;
+
+  itemRef.classList.add('item');
+  return itemRef;
+});
+
+const ingredientsList = document.querySelector('#ingredients')
+  .append(...ingridientsRef);
