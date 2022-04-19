8564b0a6fd3668fb9b71df9189c34431410179a8
Yauheni Kaspiarovich <kaspiarovichy@gmail.com>
2022-04-19 09:15:12 +0200

    add task1

--- js/task-01.js
+++ js/task-01.js
@@ -9,16 +9,21 @@
 
 // console.log("Elements:", document.querySelector(".item").lastElementChild.children.length );
 
-const numberOfCategories = document.querySelector("#categories").children;
-console.log(`Number of categories: ${numberOfCategories.length}`);
- 
+const numberOfCategories = document.querySelectorAll(".item");
+console.log('Number of categories: ', numberOfCategories.length);
 
-const numberOfCategoriesEl = document.querySelectorAll(".item")
-    .forEach((el) => console.log("Category: ", el.querySelector("h2").textContent),
-console.log("Elements: ", document.querySelector(".item").lastElementChild.children.length)
-)
+
+// const numberOfCategoriesEl = document.querySelectorAll(".item")
+//     .forEach((el) => console.log("Category: ", el.querySelector("h2").textContent),
+// console.log("Elements: ", el.querySelector("ul").children.length)
+// )
 // numberOfCategoriesEl.forEach((firstElementChild) => console.dir(firstElementChild.length) )
-
+numberOfCategories.forEach((el) => {
+    const categoriesEl = el.querySelector('h2');
+    const elementsRef = el.querySelector('ul');
+    console.log('Category: ', categoriesEl.textContent)
+    console.log('Elements: ', elementsRef.children.length);
+})
 
 //    .forEach((el) => console.dir(el)
 
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,5 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.createElement();
